a375ca2fb0b0ecb1c2c355be1470c5f287a8adb5
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-07-31 17:13:28 +0200

    updated test

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os
 import memcache
 
+print os.getenv('WERCKER_MEMCACHED_HOST')
+
 client = memcache.Client([os.getenv('WERCKER_MEMCACHED_HOST')], debug=True)
 mapping = {'foo': 'FOO', 'bar': 'BAR'}
 bad_keys = client.set_multi(mapping)
