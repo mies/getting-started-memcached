cdbe3213c762741816796f09ad558fd7f909c1d5
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-08-02 14:37:51 +0200

    print

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ print os.getenv('WERCKER_MEMCACHED_HOST')
 client = memcache.Client([os.getenv('WERCKER_MEMCACHED_HOST')], debug=True)
 mapping = {'foo': 'FOO', 'bar': 'BAR'}
 bad_keys = client.set_multi(mapping)
+print bad_keys
 print sorted(bad_keys) == ['bar', 'foo']
 print 'set_multi({0!r}) -> {1!r}'.format(mapping, bad_keys)
 
